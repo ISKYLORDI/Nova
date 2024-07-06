62389b9ee339cdd0af77d60520b63cfee82d72a8
Ling Murong <113781784+Cris-Murong@users.noreply.github.com>
2024-07-06 09:51:19 -0400

    debug trap class

--- Source/Nova/Private/AStarPathfinding.cpp
+++ Source/Nova/Private/AStarPathfinding.cpp
@@ -75,7 +75,7 @@ void AAStarPathfinding::initBlockDistances() {
 }
 
 
-void AAStarPathfinding::updateQueueCoords(int gDistance, AActor* selfActor, AActor* playerActor, TArray<AActor*> otherNPCActor) {
+void AAStarPathfinding::updateQueueCoords(int gDistance, TArray<AActor*> ignoreActors, TArray<AActor*> noIgnoreActors) {
 	TArray<FVector> dirs = {
 		FVector(width, 0, 0),FVector(-width, 0, 0),
 		FVector(0, width, 0),FVector(0, -width, 0),
@@ -83,8 +83,6 @@ void AAStarPathfinding::updateQueueCoords(int gDistance, AActor* selfActor, AAct
 		FVector(-width, width, 0),FVector(-width, -width, 0),
 	};
 	GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Black, FString::FromInt(searchedCoords.Num()));
-	TArray<AActor*> AllIgnore = otherNPCActor;
-	AllIgnore.Append(TArray<AActor*>({selfActor, playerActor}));
 	
 	for (FVector coord : searchedCoords) {
 		for (FVector dir : dirs)
@@ -116,12 +114,12 @@ void AAStarPathfinding::updateQueueCoords(int gDistance, AActor* selfActor, AAct
 			this->GetActorRotation(),
 			ETraceTypeQuery::TraceTypeQuery1,
 			false,
-			{selfActor, playerActor},
+			ignoreActors,
 			EDrawDebugTrace::None,
 			BoxHit,
 			true))
 			{
-				if (otherNPCActor.Contains(BoxHit.GetActor()))
+				if (noIgnoreActors.Contains(BoxHit.GetActor()))
 				{
 					continue;
 				}
@@ -135,7 +133,7 @@ void AAStarPathfinding::updateQueueCoords(int gDistance, AActor* selfActor, AAct
 					this->GetActorRotation(),
 					ETraceTypeQuery::TraceTypeQuery1,
 					false,
-					{selfActor, playerActor},
+					ignoreActors,
 					EDrawDebugTrace::None,
 					BoxHit,
 					true))
@@ -156,13 +154,13 @@ void AAStarPathfinding::updateQueueCoords(int gDistance, AActor* selfActor, AAct
 			this->GetActorRotation(),
 			ETraceTypeQuery::TraceTypeQuery1,
 			false,
-			{selfActor, playerActor},
+			ignoreActors,
 			EDrawDebugTrace::None,
 			BoxHit,
 			true))
 			{
 				// 扫描到了物品
-				if (otherNPCActor.Contains(BoxHit.GetActor()))
+				if (noIgnoreActors.Contains(BoxHit.GetActor()))
 				{
 					// 如果是角色 直接推出
 					continue;
@@ -176,14 +174,14 @@ void AAStarPathfinding::updateQueueCoords(int gDistance, AActor* selfActor, AAct
 				this->GetActorRotation(),
 				ETraceTypeQuery::TraceTypeQuery1,
 				false,
-				{selfActor, playerActor},
+				ignoreActors,
 				EDrawDebugTrace::None,
 				BoxHit,
 				true))
 			{
 				// 如果没有扫描到，继续扫描一格
 				// 扫描到了物品
-				if (otherNPCActor.Contains(BoxHit.GetActor()))
+				if (noIgnoreActors.Contains(BoxHit.GetActor()))
 				{
 					// 如果是角色 直接推出
 					continue;
@@ -207,7 +205,7 @@ void AAStarPathfinding::updateQueueCoords(int gDistance, AActor* selfActor, AAct
 }
 
 
-void AAStarPathfinding::solve( AActor* selfActor, AActor* playerActor, TArray<AActor*> otherNPCActor) {
+void AAStarPathfinding::solve(TArray<AActor*> ignoreActors, TArray<AActor*> noIgnoreActors) {
 	FVector nextStepCoord = startBlockCorrd;
 	
 	int currentGDistance = width;
@@ -218,7 +216,7 @@ void AAStarPathfinding::solve( AActor* selfActor, AActor* playerActor, TArray<AA
 
 		searchedCoords.Add(nextStepCoord);
 		queueCoords.Remove(nextStepCoord);
-		updateQueueCoords(currentGDistance, selfActor, playerActor, otherNPCActor);
+		updateQueueCoords(currentGDistance, ignoreActors, noIgnoreActors);
 		// UE_LOG(LogTemp, Log, TEXT("ignore actor num: %d"), IgnoreWhenTracing.Num())
 		
 		for (FVector coord : queueCoords) {
